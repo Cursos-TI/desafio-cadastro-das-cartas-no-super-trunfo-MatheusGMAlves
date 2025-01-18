f8c117950a860c21773f7f30812dcd7245e98bbe
Matheus Alves <matheusgomesmoraesalves@gmail.com>
2025-01-18 02:47:12 +0000

    FINAL

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,8 +10,8 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
-    int código_da_cidade, número_de_pontos_turísticos;
-    double população, área, PIB;
+    int codigo_da_cidade, numero_de_pontos_turisticos, populacao;
+    double area, PIB;
     char nome[50];
 
     // Cadastro das Cartas:
@@ -19,33 +19,34 @@ int main() {
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
     printf("Digite o Código da Cidade: \n");
-    scanf("%d", &código_da_cidade);
+    scanf("%d", &codigo_da_cidade);
 
     printf("Digite o número de pontos turísticos: \n");
-    scanf("%d", &número_de_pontos_turísticos);
+    scanf("%d", &numero_de_pontos_turisticos);
 
     printf("Digite a população: \n");
-    scanf("%lf", &população);
+    scanf("%d", &populacao);
 
     printf("Digite a área: \n");
-    scanf("%lf", &área);
+    scanf("%lf", &area);
 
     printf("Digite o PIB: \n");
     scanf("%lf", &PIB);
 
     printf("Digite o nome: \n");
-    scanf("%s", nome);
+    scanf("%s", &nome);
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    printf("Código da cidade: %d\n", código_da_cidade);
-    printf("Número de pontos turísticos: %d\n", número_de_pontos_turísticos);
-    printf("População: %lf\n", população);
-    printf("Área: %lf\n", área);
+    printf("Código da cidade: %d\n", codigo_da_cidade);
+    printf("Número de pontos turísticos: %d\n", numero_de_pontos_turisticos);
+    printf("População: %d\n", populacao);
+    printf("Área: %lf\n", area);
     printf("PIB: %lf\n", PIB);
     printf("Nome: %s\n", nome);
 
     return 0;
+
 }
